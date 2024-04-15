729e1a555915e8bd7cb39442648591556294c30b
Asdrubal Gonzalez Penton <132360949+asdrubalglez@users.noreply.github.com>
2024-04-15 20:59:14 +0200

    refactoring the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,23 @@
+# #!/bin/sh
+# set -e
+
+# # Run the AWS CLI command and capture output
+# aws_output=$(aws "$@")
+
+# # Set the output to the GITHUB_OUTPUT environment variable
+# echo "GITHUB_OUTPUT=$aws_output" >>$GITHUB_ENV
+
+# # Exit with the exit code of the AWS CLI command
+# exit $?
+
 #!/bin/sh
 set -e
 
 # Run the AWS CLI command and capture output
-aws_output=$(aws "$@")
+aws "$@" > /tmp/aws_output.txt
 
 # Set the output to the GITHUB_OUTPUT environment variable
-echo "GITHUB_OUTPUT=$aws_output" >>$GITHUB_ENV
+echo "GITHUB_OUTPUT=$(cat /tmp/aws_output.txt)" >> $GITHUB_ENV
 
-# Exit with the exit code of the AWS CLI command
-exit $?
+# Cleanup
+rm /tmp/aws_output.txt
