adf9c108a47a2630d4db585bd49f1b8a8ff7c3fc
Asdrubal Gonzalez Penton <132360949+asdrubalglez@users.noreply.github.com>
2024-04-15 22:07:34 +0200

    adding more parameters

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,35 @@
 #!/bin/sh
 set -e
 
+# Parse input arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --ignore-errors)
+            IGNORE_ERRORS=true
+            shift
+            ;;
+        *)
+            AWS_COMMAND_ARGS+=("$1")
+            shift
+            ;;
+    esac
+done
+
 # Run the AWS CLI command and capture output
-aws_output=$(aws "$@")
+aws_output=$(aws "${AWS_COMMAND_ARGS[@]}" 2>&1) || true
 
 # Set the output to the GITHUB_OUTPUT environment variable
 echo "GITHUB_OUTPUT=$aws_output" >> $GITHUB_ENV
 
+# Check if errors should be ignored
+if [ "$IGNORE_ERRORS" = true ]; then
+    exit 0
+fi
+
 # Exit with the exit code of the AWS CLI command
-exit $?
+if [ -n "$aws_output" ]; then
+    echo "$aws_output" >&2
+    exit 1
+fi
+exit 0
