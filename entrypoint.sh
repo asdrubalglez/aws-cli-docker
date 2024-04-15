b0662fcc2651717b41acf1d4231e2996b7e90f56
Asdrubal Gonzalez Penton <132360949+asdrubalglez@users.noreply.github.com>
2024-04-15 22:24:43 +0200

    something

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,31 +1,14 @@
 #!/bin/sh
 set -e
 
-# Parse input arguments
-while [ $# -gt 0 ]; do
-    key="$1"
-    case $key in
-        --ignore-errors)
-            IGNORE_ERRORS=true
-            shift
-            ;;
-        *)
-            AWS_COMMAND_ARGS="$AWS_COMMAND_ARGS $1"
-            shift
-            ;;
-    esac
-done
-
 # Run the AWS CLI command and capture output
-aws_output=$(aws $AWS_COMMAND_ARGS 2>&1) || true
+aws_output=$(aws "$@" 2>&1) || true
+
+# Extract the relevant information (first line)
+login_info=$(echo "$aws_output" | head -n 1)
 
 # Set the output to the GITHUB_OUTPUT environment variable
-echo "GITHUB_OUTPUT=$aws_output" >> $GITHUB_ENV
-
-# Check if errors should be ignored
-if [ "$IGNORE_ERRORS" = true ]; then
-    exit 0
-fi
+echo "GITHUB_OUTPUT=$login_info" >> $GITHUB_ENV
 
 # Exit with the exit code of the AWS CLI command
 if [ -n "$aws_output" ]; then
