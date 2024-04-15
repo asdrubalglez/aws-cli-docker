8062d4b9565572a739bd93247346dc2caba757e0
Asdrubal Gonzalez Penton <132360949+asdrubalglez@users.noreply.github.com>
2024-04-15 20:03:12 +0200

    entrypoint.sh modified

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
 set -e
 
-# Run the AWS CLI command
-aws "$@" >/tmp/aws_output.txt
+# Run the AWS CLI command and capture output
+aws_output=$(aws "$@")
 
-# Capture stdout and set it to the GITHUB_OUTPUT environment variable
-echo "GITHUB_OUTPUT=$(cat /tmp/aws_output.txt)" >>$GITHUB_ENV
+# Set the output to the GITHUB_OUTPUT environment variable
+echo "GITHUB_OUTPUT=$aws_output" >>$GITHUB_ENV
 
-# Cleanup
-rm /tmp/aws_output.txt
-
-# Exit
+# Exit with the exit code of the AWS CLI command
 exit $?
